7b416faa523fda0d840ca7e1f6fb4fc9459da7fd
hamza choubi <choubihamza10@gmail.com>
2023-02-16 07:17:07 -0800

    this is task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 /**
- * print a line with 'puts' function
+ * main - entry point
+ *
+ * Description:  print a line with 'puts' function
+ *
+ * return: always 0 (success)
  */
 int main(void)
 {
-	puts('"Programming is like building a multilingual puzzle");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
 }
