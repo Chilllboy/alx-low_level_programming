12d309a1d7d83e2344a04bed73444f4340650767
hamza choubi <choubihamza10@gmail.com>
2023-02-18 11:28:45 -0800

    this is task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main -  entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = i + 1; j <= 8; j++)
+		{
+			for (k = j + 1; k <= 9; k++)
+			{
+				putchar('0' + (i % 3));
+				putchar('0' + (j % 3));
+				putchar('0' + (k % 3));
+				if (i != 7 || j != 8 || k != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
