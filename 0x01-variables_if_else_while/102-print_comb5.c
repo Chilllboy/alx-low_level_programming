5cabd574ad616e13c80a9308f07a996d74f8ac83
hamza choubi <choubihamza10@gmail.com>
2023-02-19 01:42:11 -0800

    this is task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,15 +11,15 @@ int main(void)
 
 	for (i = 0; i <= 99; i++)
 	{
-		for (j = 1; j <= 100; j++)
+		for (j = 1; j <= 99; j++)
 		{
-			putchar(i / 10 + 48);
-			putchar(i % 10 + 48);
+			putchar((i / 10) + 48);
+			putchar((i % 10) + 48);
 			putchar(' ');
-			putchar(j / 10 + 48);
-			putchar(j % 10 + 48);
+			putchar((j / 10) + 48);
+			putchar((j % 10) + 48);
 
-			if (i != 98 || j != 100)
+			if (i != 98 || j != 99)
 			{
 				putchar(',');
 				putchar(' ');
