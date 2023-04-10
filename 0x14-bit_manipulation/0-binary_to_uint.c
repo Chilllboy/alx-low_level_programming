e6ecae4012da4c8dc000c8603e87915a8d1a719c
hamza choubi <choubihamza10@gmail.com>
2023-04-10 05:07:09 -0700

    this is task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,32 +6,31 @@
  *
  * Return: converted numbrer (Success) | 0 (Failure)
  */
-
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int chill;
-	int len, base_two;
+	unsigned int ui;
+	int x, base_two;
 
 	if (!b)
 		return (0);
 
-	chill = 0;
+	ui = 0;
 
-	for (len = 0; b[len] != '\0'; len++)
+	for (x = 0; b[x] != '\0'; x++)
 		;
 
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	for (x--, base_two = 1; x >= 0; x--, base_two *= 2)
 	{
-		if (b[len] != '0' && b[len] != '1')
+		if (b[x] != '0' && b[x] != '1')
 		{
 			return (0);
 		}
 
-		if (b[len] & 1)
+		if (b[x] & 1)
 		{
-			chill += base_two;
+			ui += base_two;
 		}
 	}
 
-	return (chill);
+	return (ui);
 }
