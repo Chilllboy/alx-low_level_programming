ab6529c5751975f10c077f123cec76da4121c5ce
hamza choubi <choubihamza10@gmail.com>
2023-04-10 08:05:40 -0700

    fixing error again

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,35 +1,26 @@
 #include "main.h"
+
 /**
- * binary_to_uint - Converts a binary to a uint
- * descreption: by (chilljoy)
- *
- * @b: the binary to convert
- *
- * Return: the number of bytes in the binary
- */
+ * binary_to_uint - converts binary to int (chilljoy)
+ * @b: binary as a string
+ * Return: decimal value of the binary
+*/
 unsigned int binary_to_uint(const char *b)
 {
-	int len = 0, base2;
 	unsigned int result = 0;
 
 	if (!b)
 		return (0);
 
-	while (b[len])
+	while (*b != '\0')
 	{
-		len++;
-	}
-	len--;
-	for (base2 = 1; len >= 0; len--, base2 *= 2)
-	{
-		if (b[len] != '1' && b[len] != '0')
-
+		if (*b == '0')
+			result = result << 1;
+		else if (*b == '1')
+			result = (result << 1) | 1;
+		else
 			return (0);
-
-		else if (b[len] & 1)
-		{
-			result += base2;
-		}
+		b++;
 	}
 	return (result);
 }
