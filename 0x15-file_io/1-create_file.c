df8507947528c0bae98809298607f4cbd5615a0f
hamza choubi <choubihamza10@gmail.com>
2023-04-10 15:48:38 -0700

    this is task 1 fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "\0";
 
-	if (write(file, text_content, strlen(text_content)) == -1)
+	if (write(fi, text_content, strlen(text_content)) == -1)
 		return (-1);
 	return (1);
 }
