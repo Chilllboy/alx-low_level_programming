af11ec88ebc62d092917f688c50af2e5648da712
hamza choubi <choubihamza10@gmail.com>
2023-04-10 15:21:28 -0700

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,24 +1,17 @@
-#ifndef
-#define
+#ifndef MAIN_H
+#define MAIN_H
 
+#include <stddef.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include<sys/stat.h>
-#include <stddef.h>
 #include <stdlib.h>
-#include <stdarg.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
-
-int create_file(const char *filename, char *text_content);
-
-size_t _strlen(char *s);
-
 int append_text_to_file(const char *filename, char *text_content);
-
-size_t _strlen2(char *s);
-
+int _putchar(char c);
+int create_file(const char *filename, char *text_content);
 
 #endif
