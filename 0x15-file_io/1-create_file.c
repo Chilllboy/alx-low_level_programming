305aac78df62333731a1cf7bc079dafe2b6e298f
hamza choubi <choubihamza10@gmail.com>
2023-04-10 15:46:54 -0700

    this is task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * create_file - create a file
+ * create_file - create a file by (chilljoy)
  * @filename: name of the file
  * @text_content: text
  * Return: 1 if successful -1 if not
@@ -10,14 +10,14 @@
 int create_file(const char *filename, char *text_content)
 {
 
-	int file;
+	int fi;
 	int flags = O_WRONLY | O_CREAT | O_TRUNC;
 	mode_t mode = S_IRUSR | S_IWUSR;
 
 	if (!filename)
 		return (-1);
-	file = open(filename, flags, mode);
-	if (file == -1)
+	fi = open(filename, flags, mode);
+	if (fi == -1)
 		return (-1);
 	if (!text_content)
 		text_content = "\0";
